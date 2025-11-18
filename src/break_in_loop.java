9a54f370260e67123668fe24e9f1c1f5a96ef91e
Yogesh Patel <yogesh@beaconrisk.co>
2025-11-18 12:03:50 +0530

    https://chatgpt.com/c/691c10ea-60d8-8322-aa49-a5d010790d53

---
+++ src/break_in_loop.java
@@ -0,0 +1,18 @@
+public class break_in_loop {
+    public static void main(String[] args) {
+
+
+        for (
+                int i = 1;
+                i <= 10; i++) {
+            if (i == 5) {
+                break;     // loop stops when i becomes 5
+            }
+            System.out.println(i);//output: 1
+            //2
+            //3
+            //4
+
+        }
+    }
+}

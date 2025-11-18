f2f3fea3079d62d8232d3ae923c416f1572cf9f4
Yogesh Patel <yogesh@beaconrisk.co>
2025-11-18 11:38:08 +0530

    ok

--- src/nested_loop.java
+++ src/nested_loop.java
@@ -1,4 +1,4 @@
-public class NestedLoopExample {
+public class nested_loop {
     public static void main(String[] args) {
         for (int i = 1; i <= 3; i++) {        // Outer Loop
             for (int j = 1; j <= 2; j++) {    // Inner Loop
